a8777e4d3bc295a53b35646f8b2f685812fd7899
alexrefshauge <nk02hb@aau.student.dk>
2025-04-16 23:10:00 +0200

    lsp

--- init.lua
+++ init.lua
@@ -1,2 +1,31 @@
 require("alexref.remap")
-require("alexref.lazy")
+require("alexref.lazy_init")
+
+local augroup = vim.api.nvim_create_augroup
+local AlexRefGroup = augroup('AlexRef', {}) 
+
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = "a"
+vim.opt.showmode = false
+-- vim.opt.colorcolumn = "80"
+local autocmd = vim.api.nvim_create_autocmd
+autocmd('LspAttach', {
+	group = AlexRefGroup,
+	callback = function(e) 
+		vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+		vim.keymap.set("n","K", function() vim.lsp.buf.hover() end, opts)
+	end
+})
+
+vim.diagnostic.enable(true)
+vim.cmd("colorscheme cyberdream")
--- remap.lua
+++ remap.lua
@@ -1,3 +1,14 @@
 vim.g.mapleader = " "
+-- vim.g.maplocalleader = "\\"
 vim.keymap.set("n", "<leader>o", vim.cmd.Ex)
+
+-- move line
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv'")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv'")
+
+-- splits
+vim.keymap.set("n", "<leader>sj", function() vim.api.nvim_open_win(0, true, {split = 'below'}) end)
+vim.keymap.set("n", "<leader>sk", function() vim.api.nvim_open_win(0, true, {split = 'above'}) end)
+vim.keymap.set("n", "<leader>sl", function() vim.api.nvim_open_win(0, true, {split = 'right'}) end)
+vim.keymap.set("n", "<leader>sh", function() vim.api.nvim_open_win(0, true, {split = 'left'}) end)
 
