7a10fd9fc2479b8b474804a9df6ccc63afd5b849
alexrefshauge <nk02hb@aau.student.dk>
2025-05-07 22:38:13 +0200

    harpoon

--- lazy/harpoon.lua
+++ lazy/harpoon.lua
@@ -4,15 +4,20 @@ return {
 		"nvim-lua/plenary.nvim"
 	},
 	config = function()
-		require("harpoon"):setup()
+		local harpoon = require("harpoon")
+
+		harpoon:setup()
+
+		vim.keymap.set("n", "<leader>A", function() harpoon:list():prepend() end)
+		vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+		vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+		vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+		vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+		vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+		vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+		vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
+		vim.keymap.set("n", "<leader><C-t>", function() harpoon:list():replace_at(2) end)
+		vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(3) end)
 	end,
-	keys = {
-		{ "<leader>A", function() require("harpoon"):list():append() end, desc = "harpoon file", },
-		{ "<leader>a", function() require("harpoon"):list():add() end, desc = "harpoon file", },
-		{ "<leader>1", function() require("harpoon"):list():select(1) end, desc = "harpoon to file 1", },
-		{ "<leader>2", function() require("harpoon"):list():select(2) end, desc = "harpoon to file 2", },
-		{ "<leader>3", function() require("harpoon"):list():select(3) end, desc = "harpoon to file 3", },
-		{ "<leader>4", function() require("harpoon"):list():select(4) end, desc = "harpoon to file 4", },
-		{ "<leader>5", function() require("harpoon"):list():select(5) end, desc = "harpoon to file 5", },
-	},
 }
