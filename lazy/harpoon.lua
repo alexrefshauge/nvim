ae2000b3b2c2364b9ce139136e26849e0963d316
alexrefshauge <nk02hb@aau.student.dk>
2025-05-08 22:53:14 +0200

    harpoon select

--- lazy/harpoon.lua
+++ lazy/harpoon.lua
@@ -1,5 +1,6 @@
 return {
 	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
 	dependencies = {
 		"nvim-lua/plenary.nvim"
 	},
@@ -12,10 +13,10 @@ return {
 		vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 		vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-		vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-		vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-		vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-		vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+		vim.keymap.set("n", "1", function() harpoon:list():select(1) end)
+		vim.keymap.set("n", "2", function() harpoon:list():select(2) end)
+		vim.keymap.set("n", "3", function() harpoon:list():select(3) end)
+		vim.keymap.set("n", "4", function() harpoon:list():select(4) end)
 		vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
 		vim.keymap.set("n", "<leader><C-t>", function() harpoon:list():replace_at(2) end)
 		vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(3) end)
